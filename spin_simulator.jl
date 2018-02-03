1365383ed1630c5516bdc28901478ff43a5ad9c3
shun <shun@shun-no-MacBook-Pro.local>
2018-02-03 21:09:04 +0900

    tura

--- spin_simulator.jl
+++ spin_simulator.jl
@@ -1,5 +1,7 @@
 # Author:Shun Arahata
 using PyPlot
+import Elliptic.Jacobi
+import Elliptic.Elliptic
 const Ix = 1.9
 const Iy = 1.6
 const Iz = 2.0
@@ -60,18 +62,63 @@ function q_differential(t, qandω)
     dq1 = 1/2 * (q0 * ωx + -q3 * ωy + q2 * ωz)
     dq2 = 1/2 * (q3 * ωx + q0 * ωy - q1 * ωz)
     dq3 = 1/2 * (-q2 * ωx + q1 * ωy + q0 * ωz)
-    return [dq0, dq1, dq2, dq3, 0.0, 0.0, 0.0]
+    norm = sqrt(dq0^2 + dq1^2 + dq2^2 + dq3^2)
+    return [dq0/norm, dq1/norm, dq2/norm, dq3/norm, 0.0, 0.0, 0.0]
+end
+
+function make_theoretical_answer()
+    """
+    :param ω: list of ωx, ωy, ωz
+    """
+    ω_list = Array{Float64, 2}(STEPNUM+2,3)
+    ω_list[1, :] = ωb'
+    E = 0.5 * (Ix * ωb[1]^2 + Iy * ωb[2]^2 + Iz * ωb[3]^2)
+    L2 = Ix^2 * ωb[1]^2 + Iy^2 * ωb[2]^2 + Iz^2 * ωb[3]^2
+    lambda = sqrt((L2 - 2E * Iy)*(Iz - Ix)/(Ix * Iy * Iz))
+    println((2E * Iz - L2))
+    println((L2 - 2E * Iy))
+    k2 = ((Ix - Iy)*(2E * Iz - L2))/((Iz - Ix)*(L2 - 2E * Iy))
+    println(k2)
+    k = sqrt(k2)
+    #find  period T
+
+    # find t when omega_x = 0
+    t_prime = 0
+    abs_omega_x = 100
+    for i in 0:STEPNUM
+        t = i * STEP
+        omega_x = sqrt((2E*Iz - L2)/(Ix * (Iz - Ix))) * Jacobi.sn(lambda * t, k^2)
+        if abs(omega_x) < abs_omega_x
+            println("mo")
+            abs_omega_x = abs(omega_x)
+            t_prime = t
+        end
+    end
+    for i in 0:STEPNUM
+        t = i * STEP - t_prime
+        omega_x = sqrt((2E*Iz - L2)/(Ix * (Iz - Ix))) * Jacobi.sn(lambda * t, k^2)
+        omega_y = sqrt((2E*Iz - L2)/(Iy * (Iz - Iy))) * Jacobi.cn(lambda * t, k^2)
+        omega_z = sqrt((2E*Iy - L2)/(Iz * (Iy - Iz))) * Jacobi.dn(lambda * t, k^2)
+        ω_list[i+2, :] = [omega_x, omega_y, omega_z]
+    end
+    return ω_list
 end
 
 function plot(time, ω, q)
+    #=
     fig = figure()
     ax = fig[:add_subplot](111)
     ax[:plot](time, ω[:,1], label=L"$\omega_x$")
     ax[:plot](time, ω[:,2], label=L"$\omega_y$")
     ax[:plot](time, ω[:,3], label=L"$\omega_z$")
     ax[:set_xlim]([0, last(time)])
+    ax[:set_xlabel]("time [sec]")
+    ax[:set_ylabel](L"$\omega$ [rad/s]")
     legend(loc = "best", fontsize=15)
-    PyPlot.plt[:show]()
+    # PyPlot.plt[:show]()
+    PyPlot.plt[:savefig]("omega.pgf")
+    =#
+    #=
     fig = figure()
     ax = fig[:add_subplot](111)
     ax[:plot](time, q[:,1], label=L"$q_0$")
@@ -79,7 +126,35 @@ function plot(time, ω, q)
     ax[:plot](time, q[:,3], label=L"$q_2$")
     ax[:plot](time, q[:,4], label=L"$q_3$")
     ax[:set_xlim]([0, last(time)])
+    ax[:set_xlabel]("time [sec]")
+    ax[:set_ylabel]("Quaternion")
     legend(loc = "best", fontsize=15)
+    PyPlot.plt[:show]()
+    PyPlot.plt[:savefig]("quaternion.pgf")
+    =#
+    fig = figure()
+    theo = make_theoretical_answer()
+    ax = fig[:add_subplot](311)
+    ax[:plot](time, ω[:,1], label="Numerical")
+    ax[:plot](time, theo[:,1], label="Theoretical")
+    ax[:set_xlim]([0, last(time)])
+    ax[:set_xlabel]("time [sec]")
+    ax[:set_ylabel](L"$\omega_x$")
+    legend(loc = 1)
+    ax = fig[:add_subplot](312)
+    ax[:plot](time, ω[:,2], label="Numerical")
+    ax[:plot](time, theo[:,2], label="Theoretical")
+    ax[:set_xlim]([0, last(time)])
+    ax[:set_xlabel]("time [sec]")
+    ax[:set_ylabel](L"$\omega_y$")
+    legend(loc = 1)
+    ax = fig[:add_subplot](313)
+    ax[:plot](time, ω[:,3], label="Numerical")
+    ax[:plot](time, theo[:,3], label="Theoretical")
+    ax[:set_xlim]([0, last(time)])
+    ax[:set_xlabel]("time [sec]")
+    ax[:set_ylabel](L"$\omega_z$")
+    legend(loc = 1)
     PyPlot.plt[:show]()
 end
 
