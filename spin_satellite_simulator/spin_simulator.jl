08bacdbf9b7472f7d0c128c587290bc6d4cee25a
shun <shun@shun-no-MacBook-Pro.local>
2018-02-09 15:01:41 +0900

    tura

--- spin_satellite_simulator/spin_simulator.jl
+++ spin_satellite_simulator/spin_simulator.jl
@@ -10,8 +10,8 @@ const ωs = rpm2radpersec(17)
 const STEPNUM = 10000
 const STEP = 0.01
 
-q_initial = [1.0, 0.0, 0.0, 0.0]
-ωb = [0.1, ωs + 0.1, 0.0]
+q_initial = [1.0; 0.0; 0.0; 0.0]
+ωb = [0.1; ωs + 0.1; 0.0]
 
 function runge_kutta(f, t, x, step)
     #=　Fourth-order Runge-Kutta method.
@@ -31,45 +31,29 @@ function runge_kutta(f, t, x, step)
     return sum
 end
 
-function ω_differential(t, ω)
-    #= differential equation of ω.
-
-    :param t: time(not related)
-    :param ω: list of ωx, ωy, ωz
-    :return: differential of ω list
-    =#
-    ωx = (Iy - Iz)/Ix * ω[2] * ω[3]
-    ωy = (Iz - Ix)/Iy * ω[3] * ω[1]
-    ωz = (Ix - Iy)/Iz * ω[1] * ω[2]
-    return [ωx, ωy, ωz]
-end
-
-function q_differential(t, qandω)
-    #= differential equation of quaternion.
-
-    :param t: time(not related)
-    :param qandω: list of quaternion + ω
-    :return: differential of quaternion
-    =#
-    q0 = qandω[1]
-    q1 = qandω[2]
-    q2 = qandω[3]
-    q3 = qandω[4]
-    ωx = qandω[5]
-    ωy = qandω[6]
-    ωz = qandω[7]
+function differential_eq(t, x)
+    q0 = x[1]
+    q1 = x[2]
+    q2 = x[3]
+    q3 = x[4]
+    ωx = x[5]
+    ωy = x[6]
+    ωz = x[7]
     dq0 = 1/2 * (-q1 * ωx + -q2 * ωy - q3 * ωz)
     dq1 = 1/2 * (q0 * ωx + -q3 * ωy + q2 * ωz)
     dq2 = 1/2 * (q3 * ωx + q0 * ωy - q1 * ωz)
     dq3 = 1/2 * (-q2 * ωx + q1 * ωy + q0 * ωz)
     norm = sqrt(dq0^2 + dq1^2 + dq2^2 + dq3^2)
-    return [dq0/norm, dq1/norm, dq2/norm, dq3/norm, 0.0, 0.0, 0.0]
+    omega_x = (Iy - Iz)/Ix * ωy * ωz
+    omega_y = (Iz - Ix)/Iy * ωz * ωx
+    omega_z = (Ix - Iy)/Iz * ωx * ωy
+    return [dq0/norm; dq1/norm; dq2/norm; dq3/norm; omega_x; omega_y; omega_z]
 end
 
 function make_theoretical_answer()
-    """
+    #=
     :param ω: list of ωx, ωy, ωz
-    """
+    =#
     ω_list = Array{Float64, 2}(STEPNUM+2,3)
     ω_list[1, :] = ωb'
     E = 0.5 * (Ix * ωb[1]^2 + Iy * ωb[2]^2 + Iz * ωb[3]^2)
@@ -157,12 +141,11 @@ end
 
 function main()
     x = Array{Float64, 2}(STEPNUM+2,7)
-    x[1, 5:7] = ωb'
-    x[1, 1:4] = q_initial'
+    x[1, 5:7] = ωb
+    x[1, 1:4] = q_initial
     time = zeros(STEPNUM+2)
     for i in 0:STEPNUM
-        x[i+2, 1:4] = x[i+1, 1:4] + runge_kutta(q_differential, i * STEP, x[i+1,:], STEP)[1:4]
-        x[i+2, 5:7] = x[i+1, 5:7] + runge_kutta(ω_differential, i * STEP, x[i+1,5:7], STEP)
+        x[i+2, :] = x[i+1, :] + runge_kutta(differential_eq, i * STEP, x[i+1,:], STEP)
         time[i+2] = (i+1) * STEP
     end
     # writecsv("quaternion.csv",q_list)
